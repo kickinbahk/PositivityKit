facfceb99800dbc0449f06ef4e1ac7b8d1afc1e4
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-05-17 08:29:19 -0700

    add selfie button to UI

--- PositivityKit/MainScreenViewController.swift
+++ PositivityKit/MainScreenViewController.swift
@@ -12,6 +12,8 @@ class MainScreenViewController: UIViewController {
 
   let selfieButton = UIButton()
   let dailyActivityButton = UIButton()
+  let buttonsContainer = UIView()
+  
   var backgroundImage = UIImageView()
   
   override func viewDidLoad() {
@@ -22,26 +24,50 @@ class MainScreenViewController: UIViewController {
   }
 
   func  updateUI() {
+    // Add any necessary properties to the UI elements
     let image = UIImage(named: "background-image")
     backgroundImage = UIImageView(image: image)
     
     selfieButton.backgroundColor = UIColor.yellow
     selfieButton.setTitle("Take a Selfie", for: .normal)
+    selfieButton.setTitleColor(UIColor.black, for: .normal)
     
     dailyActivityButton.backgroundColor = UIColor.yellow
     dailyActivityButton.setTitle("Daily Activity", for: .normal)
     
     view.addSubview(backgroundImage)
+    view.addSubview(buttonsContainer)
+    // Use a container to keep the buttons together in the layout
+    buttonsContainer.addSubview(selfieButton)
+    buttonsContainer.addSubview(dailyActivityButton)
     
     addConstriants()
   }
   
   func addConstriants() {
+    // layout the elements in the view
     backgroundImage.translatesAutoresizingMaskIntoConstraints = false
     backgroundImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     backgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-    backgroundImage.contentMode = .scaleAspectFill
+    backgroundImage.contentMode = .scaleAspectFill // Center image, fill space, and crop any excess areas
+    
+    buttonsContainer.translatesAutoresizingMaskIntoConstraints = false
+    buttonsContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    buttonsContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    buttonsContainer.heightAnchor.constraint(equalToConstant: view.bounds.height / 3).isActive = true
+    buttonsContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+    buttonsContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+    
+    selfieButton.translatesAutoresizingMaskIntoConstraints = false
+    selfieButton.leadingAnchor.constraint(equalTo: buttonsContainer.leadingAnchor,
+                                          constant: 20).isActive = true
+    selfieButton.trailingAnchor.constraint(equalTo: buttonsContainer.trailingAnchor,
+                                           constant: -20).isActive = true
+    selfieButton.topAnchor.constraint(equalTo: buttonsContainer.topAnchor, constant: 20).isActive = true
+    selfieButton.bottomAnchor.constraint(equalTo: buttonsContainer.bottomAnchor, constant: -20).isActive = true
+    
+    
   }
 
 }
